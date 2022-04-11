578bdc41152d9aae456e90f4c80615097b8d26b8
joshuachio <jchio@gatech.edu>
2022-04-11 18:18:14 -0400

    Added a keyboard with used characters

--- Jordle.java
+++ Jordle.java
@@ -31,6 +31,7 @@ import javafx.geometry.Insets;
 import javafx.geometry.HPos;
 import javafx.geometry.Pos;
 import javafx.scene.layout.GridPane;
+import java.util.Hashtable;
 
 public class Jordle extends Application {
 
@@ -39,11 +40,12 @@ public class Jordle extends Application {
     boolean check = true;
     Rectangle[][] rectArr = new Rectangle[6][5];
     Label[][] labArr = new Label[6][5];
+    Hashtable<Character, Label> letterTab = new Hashtable<Character, Label>();
+    Hashtable<Character, Rectangle> rectTab = new Hashtable<Character, Rectangle>();
 
     Random r = new Random();
     int rand = r.nextInt(Words.list.size());
-    // String word = Words.list.get(rand);
-    String word = "Booby";
+    String word = Words.list.get(rand);
     Stage errorStage = new Stage();
     Label inGameText = new Label();
 
@@ -58,18 +60,18 @@ public class Jordle extends Application {
         Button restart = new Button("Restart");
         Button instructions = new Button("How to Play");
 
-        title.setFont(new Font("Arial Bold", 50));
+        title.setFont(new Font("Clear Sans", 50));
 
         titleBox.getChildren().add(instructions);
         titleBox.getChildren().add(title);
         titleBox.getChildren().add(restart);
         titleBox.setBackground(new Background(new BackgroundFill(Color.WHITE, null, null)));
-        titleBox.setPadding(new Insets(30, 25, 30, 25));
+        titleBox.setPadding(new Insets(30, 25, 0, 25));
         titleBox.setAlignment(Pos.CENTER);
 
         //Main GridPane body
 
-        GridPane pane = new GridPane();
+        GridPane gamePane = new GridPane();
 
         for (int i = 0; i < 6; ++i) {
             for (int u = 0; u < 5; ++u) {
@@ -77,19 +79,55 @@ public class Jordle extends Application {
                 rect.setFill(Color.WHITE);
                 rect.setStrokeWidth(1.5);
                 rect.setStroke(Color.color(.80, 0.80, 0.80, 1.0));
-                pane.add(rect, u, i);
+                gamePane.add(rect, u, i);
                 Label text =  new Label(" ");
-                pane.add(text, u, i);
-                text.setFont(Font.font("Monospaced", FontWeight.BOLD, 30));
-                pane.setHalignment(text, HPos.CENTER);
+                gamePane.add(text, u, i);
+                text.setFont(Font.font("Clear Sans", FontWeight.BOLD, 30));
+                gamePane.setHalignment(text, HPos.CENTER);
                 rectArr[i][u] = rect;
                 labArr[i][u] = text;
             }
         }
 
-        pane.setAlignment(Pos.CENTER);
-        pane.setHgap(7);
-        pane.setVgap(7);
+        gamePane.setAlignment(Pos.CENTER);
+        gamePane.setHgap(7);
+        gamePane.setVgap(7);
+
+        //A-Z GridPane
+
+        GridPane alphaPane = new GridPane();
+
+        Character[] letters = new Character[30];
+        String az = "QWERTYUIOP ASDFGHJKL  ZXCVBNM ";
+        for (int i = 0; i < az.length(); ++i) {
+                letters[i] = az.charAt(i);
+        }
+        int letter = 0;
+        for (int i = 0; i < 3; ++i) {
+            for (int u = 0; u < 10; ++u) {
+                Rectangle rect = new Rectangle(0, 0, 30, 30);
+                rect.setFill(Color.WHITE);
+                rect.setStrokeWidth(1.5);
+                if (letter == 10 || letter == 20 || letter == 21 || letter == 29) {
+                    rect.setStroke(Color.WHITE);
+                } else {
+                    rect.setStroke(Color.color(.80, 0.80, 0.80, 1.0));
+                }
+                alphaPane.add(rect, u, i);
+                Label text =  new Label(letters[letter].toString());
+                alphaPane.add(text, u, i);
+                text.setFont(Font.font("Clear Sans", FontWeight.BOLD, 16));
+                alphaPane.setHalignment(text, HPos.CENTER);
+                letterTab.put(Character.toLowerCase(letters[letter]), text);
+                rectTab.put(Character.toLowerCase(letters[letter]), rect);
+                ++letter;
+            }
+        }
+        
+        alphaPane.setAlignment(Pos.CENTER);
+        alphaPane.setHgap(5);
+        alphaPane.setVgap(5);
+        alphaPane.setPadding(new Insets(10, 25, 10, 25));
 
         //Footer HBox
 
@@ -99,16 +137,17 @@ public class Jordle extends Application {
 
         footerBox.getChildren().add(inGameText);
         footerBox.setAlignment(Pos.CENTER);
-        footerBox.setPadding(new Insets(20));
+        // footerBox.setPadding(new Insets(10, 0, 50, 0));
         footerBox.setBackground(new Background(new BackgroundFill(Color.WHITE, null, null)));
         
         //VBox combining all the boxes
 
-        VBox vbox = new VBox(25);
+        VBox vbox = new VBox(10);
 
         vbox.getChildren().add(titleBox);
-        vbox.getChildren().add(pane);
         vbox.getChildren().add(footerBox);
+        vbox.getChildren().add(gamePane);
+        vbox.getChildren().add(alphaPane);
         vbox.setBackground(new Background(new BackgroundFill(Color.WHITE, null, null)));
 
         //Scene and Stage creation
@@ -126,7 +165,7 @@ public class Jordle extends Application {
         FlowPane flowPane = new FlowPane();
 
         Label instructionsText = new Label("Welcome to Jordle!\n\nGuess the mystery 5-letter word in 6 tries. " +
-                                    "Each guess must be a valid five-letter word. Hit the enter button to submit. " +
+                                    "Each guess must be a valid five-letter word. Hit the \"ENTER\" button to submit. " +
                                     "After each guess, the color of the tiles will change to show how close your guess " +
                                     "was to the word.");
 
@@ -135,11 +174,11 @@ public class Jordle extends Application {
         instructionsText.setMaxWidth(230);
         instructionsText.setTranslateX(15);
         instructionsText.setTranslateY(15);
-        instructionsText.setFont(new Font("nyt-franklin", 14));
+        instructionsText.setFont(new Font("Clear Sans", 14));
         flowPane.getChildren().add(instructionsText);
 
         introStage.setTitle("Instructions");
-        introStage.setScene(new Scene(flowPane, 260, 190));
+        introStage.setScene(new Scene(flowPane, 260, 210));
         if (check) {
             introStage.show();
         }
@@ -166,15 +205,14 @@ public class Jordle extends Application {
         Label errorText = new Label("Not enough letters");
         errorText.setWrapText(true);
         errorText.setTextAlignment(TextAlignment.CENTER);
-        // errorText.setMaxWidth(150);
-        errorText.setMaxHeight(30);
-        errorText.setTranslateX(15);
-        errorText.setTranslateY(15);
+        // errorText.setMaxHeight(30);
+        // errorText.setTranslateX(15);
+        // errorText.setTranslateY(15);
         errorText.setFont(new Font("nyt-franklin", 14));
         FlowPane errorPane = new FlowPane();
         errorPane.getChildren().add(errorText);
-        errorPane.setPadding(new Insets(20, 50, 30, 20));
-        Scene errorScene = new Scene(errorPane);
+        errorPane.setPadding(new Insets(30, 70, 30, 30));
+        Scene errorScene = new Scene(errorPane, 175, 90);
         errorStage.setScene(errorScene);
         return errorStage;
     }
@@ -198,6 +236,7 @@ public class Jordle extends Application {
                 }
             } else if (e.getCode() == KeyCode.ENTER) {
                 if (column == 5) {
+                    System.out.println(word);
                     ArrayList<Character> inputArr = new ArrayList();
                     ArrayList<Character> wordArr = new ArrayList();
                     int counter = 0;
@@ -205,21 +244,28 @@ public class Jordle extends Application {
                         wordArr.add(word.charAt(i));
                         inputArr.add(labArr[row][i].getText().toLowerCase().charAt(0));
                     }
-                    System.out.println(inputArr.toString());
-                    System.out.println(wordArr.toString());
                     for (int i = 0; i < 5; ++i) {
                         if (wordArr.get(i) == inputArr.get(i)) {
                             rectArr[row][i].setFill(new Color(115.0 / 255.0, 171.0 / 255.0, 120.0 / 255.0, 1));
                             rectArr[row][i].setStroke(Color.WHITE);
                             labArr[row][i].setTextFill(Color.WHITE);
+                            rectTab.get(Character.valueOf((char) inputArr.get(i))).setFill(new Color(115.0 / 255.0, 171.0 / 255.0, 120.0 / 255.0, 1));
+                            rectTab.get(Character.valueOf((char) inputArr.get(i))).setStroke(Color.WHITE);
+                            letterTab.get(Character.valueOf((char) inputArr.get(i))).setTextFill(Color.WHITE);
                         } else if (wordArr.contains(inputArr.get(i))) {
                             rectArr[row][i].setFill(new Color(201.0 / 255.0, 180.0 / 255.0, 88.0 / 255.0, 1));
                             rectArr[row][i].setStroke(Color.WHITE);
                             labArr[row][i].setTextFill(Color.WHITE);
+                            rectTab.get(Character.valueOf((char) inputArr.get(i))).setFill(new Color(201.0 / 255.0, 180.0 / 255.0, 88.0 / 255.0, 1));
+                            rectTab.get(Character.valueOf((char) inputArr.get(i))).setStroke(Color.WHITE);
+                            letterTab.get(Character.valueOf((char) inputArr.get(i))).setTextFill(Color.WHITE);
                         } else {
                             rectArr[row][i].setFill(Color.GRAY);
                             rectArr[row][i].setStroke(Color.WHITE);
                             labArr[row][i].setTextFill(Color.WHITE);
+                            rectTab.get(Character.valueOf((char) inputArr.get(i))).setFill(Color.GRAY);
+                            rectTab.get(Character.valueOf((char) inputArr.get(i))).setStroke(Color.WHITE);
+                            letterTab.get(Character.valueOf((char) inputArr.get(i))).setTextFill(Color.WHITE);
                         }
                         if (rectArr[row][i].getFill().equals(new Color(115.0 / 255.0, 171.0 / 255.0, 120.0 / 255.0, 1))) {
                             ++counter;
